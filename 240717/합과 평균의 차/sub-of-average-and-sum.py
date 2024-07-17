953a13ebeafca40c644fdbe96c7b255e01f97ef9
burningdust <hermony285@gmail.com>
2024-07-17 21:10:05 +0900

    Title: 합과 평균의 차, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240717/합과 평균의 차/sub-of-average-and-sum.py
+++ 240717/합과 평균의 차/sub-of-average-and-sum.py
@@ -1,5 +1,5 @@
 arr = input().split()
 a,b,c = int(arr[0]), int(arr[1]), int(arr[2])
 sum_arr = a+b+c
-mean_arr = sum_arr/len(arr)
+mean_arr = int(sum_arr/3)
 print(sum_arr, mean_arr, sum_arr - mean_arr, sep="\n")
