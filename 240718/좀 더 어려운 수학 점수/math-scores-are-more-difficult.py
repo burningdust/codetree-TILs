5cfa3086362a83bd4de85ff26e5b757e0956f71f
burningdust <hermony285@gmail.com>
2024-07-18 21:05:40 +0900

    Title: 좀 더 어려운 수학 점수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240718/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,13 @@
+A = input().split()
+A_math, A_eng = int(A[0]), int(A[1])
+B = input().split()
+B_math, B_eng = int(B[0]), int(A[1])
+
+if A_math>B_math :
+    print("A")
+elif A_math<B_math :
+    print("B")
+elif A_eng>B_eng :
+    print("A")
+else :
+    print("B")
