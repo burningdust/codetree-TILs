9d8d76339c054295abcd1f1b782385505dc99bb9
burningdust <hermony285@gmail.com>
2024-07-19 22:35:58 +0900

    Title: 높은 수에서 낮은 수까지, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240719/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,11 @@
+arr = input().split()
+a,b = int(arr[0]), int(arr[1])
+
+if a>=b :
+    for i in range(a, b-1, -1):
+        print(i, end=" ")
+        i -= 1
+else :
+    for i in range(b, a-1, -1):
+        print(i, end=" ")
+        i -= 1
