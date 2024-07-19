ee4aa872559238fa48bf360f9524e221ff6177cd
burningdust <hermony285@gmail.com>
2024-07-19 22:29:01 +0900

    Title: a/b 출력, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/a/b 출력/a-divide-b.py
+++ 240719/a/b 출력/a-divide-b.py
@@ -1,10 +1,10 @@
 arr = input().split()
 a,b = int(arr[0]), int(arr[1])
 
-rate = a/b
-print(int(rate), end=".")
+print(f"{a//b}.", end="")
 
+a %= b
 for _ in range(20):
-    rate -= int(rate)
-    rate *= 10
-    print(int(rate), end="")
+    a *= 10
+    print(a//b, end="")
+    a %= b
