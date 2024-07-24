25ab0ddb99c508a81b7a9ca25b20fd726f4dd877
burningdust <hermony285@gmail.com>
2024-07-24 22:07:37 +0900

    Title: 별 그리기, Time: 157ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240724/별 그리기/star-drawing.py
@@ -0,0 +1,14 @@
+n = int(input())
+cnt = n-1
+
+for i in range(2*n-1):
+    for _ in range(cnt):
+        print(" ", end="")
+    for _ in range(2*(n-cnt)-1):
+        print("*", end="")
+    print()
+
+    if i<n-1:
+        cnt -= 1
+    else:
+        cnt += 1
