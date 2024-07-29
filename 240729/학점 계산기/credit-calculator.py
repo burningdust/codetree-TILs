47a6dc5c32a3a3317c7b70b23390e5c41a7f1872
burningdust <hermony285@gmail.com>
2024-07-29 20:49:36 +0900

    Title: 학점 계산기, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240729/학점 계산기/credit-calculator.py
+++ 240729/학점 계산기/credit-calculator.py
@@ -1,9 +1,9 @@
 n = int(input())
 score = list(map(float, input().split()))
 print(f"{sum(score)/n:.1f}")
-if sum(score)/n > 4.0:
+if sum(score)/n >= 4.0:
     print("Perfect")
-elif sum(score)/n > 3.0:
+elif sum(score)/n >= 3.0:
     print("Good")
 else:
     print("Poor")
