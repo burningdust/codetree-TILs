cb9b4c554897b555952521beebea6d229811ebd8
burningdust <hermony285@gmail.com>
2024-08-05 20:32:56 +0900

    Title: Run Length 인코딩, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240805/Run Length 인코딩/run-length-encoding.py
+++ 240805/Run Length 인코딩/run-length-encoding.py
@@ -1,9 +1,8 @@
 A = input()
 cnt = 1
-arr = []
+arr = [A[0]]
 
-arr.append(A[0])
-for i in range(1, len(A)-1):
+for i in range(1, len(A)):
     if A[i]==A[i-1]:
         cnt += 1
     else:
@@ -11,13 +10,8 @@ for i in range(1, len(A)-1):
         arr.append(A[i])
         cnt = 1
 
-if A[-2]!=A[-1]:
-    arr.append(A[-1])
-    cnt = 1
-else:
-    cnt += 1
 arr.append(cnt)
-
+        
 print(len(arr))
 for elem in arr:
     print(elem, end="")
