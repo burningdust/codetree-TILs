d9c0a576d4a4003fdf8b6ff08b086aac2d8a221f
Narin Kim <narin_kim@ewha.ac.kr>
2024-09-26 20:25:58 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 5090ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240926/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
+++ 240926/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -1,9 +1,10 @@
 def maximum(n, arr):
-    if n==1:
+    if n==2:
         if arr[0] < arr[1]:
             return arr[1]
         else:
             return arr[0]
+
     if maximum(n-1, arr) < arr[n-1]:
         return arr[n-1]
     else:
